4364907427e75a8397ffbc05f51ef59270902f46
RizAPS2106 <91816008+RizAPS2106@users.noreply.github.com>
2025-02-27 11:40:34 +0700

    adjusting

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="icon" type="image/x-icon" href=" {{ asset('images/defect.ico') }}">
-    <title>NDS DEFECT</title>
+    <title>SCAN NDS DEFECT</title>
 
     @include('layouts.link')
 
